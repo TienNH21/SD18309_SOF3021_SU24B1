a73860991ad1ff53fc6d0257e2def880d2382cc9
TienNH21 <tiennh21@fpt.edu.vn>
2024-05-20 14:55:14 +0700

    Validate

--- src/main/java/com/example/demo/entities/SanPham.java
+++ src/main/java/com/example/demo/entities/SanPham.java
@@ -1,5 +1,8 @@
 package com.example.demo.entities;
 
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +13,14 @@ import lombok.Setter;
 @Getter @Setter
 public class SanPham {
     private Integer id;
+
+    @NotBlank(message = "Không được để trống")
     private String ma;
+
+    @NotBlank
     private String ten;
+
+    @NotNull
+    @Digits(integer = 1, fraction = 0)
     private int trangThai;
 }
